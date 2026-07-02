e96615aee3035e2271dde0e32d1d23ed5d4263a9
Guillermo Manns <guillermomanns@gmail.com>
2026-07-02 16:42:07 -0400

    refactor: centralize LLM communication in Brain.chat

--- brain.py
+++ brain.py
@@ -6,22 +6,28 @@ class Brain:
 
     def __init__(self):
         self.model = config.MODEL
+        self.temperature = config.TEMPERATURE
 
     def ask(self, messages, tools=None):
+
+        return self.chat(
+            messages,
+            tools
+        )
+
+    def continue_chat(self, messages):
+
+        return self.chat(messages)
+    
+    def chat(self, messages, tools=None):
 
         response = chat(
             model=self.model,
             messages=messages,
-            tools=tools
-        )
-
-        return response.message
-
-    def continue_chat(self, messages):
-
-        response = chat(
-            model=self.model,
-            messages=messages
+            tools=tools,
+            options={
+                "temperature": self.temperature
+            }
         )
 
         return response.message
